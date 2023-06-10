dc59d3a4803ce28ed13217457f2fc82efd0827f6
109400752 <109400752+Amit-A2Z@users.noreply.github.com>
2023-06-10 09:54:54 +0530

    remove unneccesary files

--- samples/spring/src/main/java/com/sap/cp/cf/demoapps/SecurityConfiguration.java
+++
@@ -1,8 +0,0 @@
-package com.sap.cp.cf.demoapps;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class SecurityConfiguration {
-	// TODO see https://github.com/SAP-samples/cloud-cf-product-list/blob/teched2019/docs/09_secure/Spring.md
-}
